fc14396e324dea860bfc2c360df35a0e1f1656bd
lhgfrontis <85724878+AmaneKanata@users.noreply.github.com>
2023-06-21 21:25:56 +0900

    c# 클라이언트 자동 생성 코드 수정

--- src/ProtoBuf/Templates/PacketHandler.cs
+++ src/ProtoBuf/Templates/PacketHandler.cs
@@ -2,33 +2,36 @@ using Google.Protobuf;
 using System;
 using System.Collections.Generic;
 
-public class PacketHandler
+namespace Framework.Network
 {
-    public Dictionary<ushort, Action<IMessage>> Handlers = new();
+    public class PacketHandler
+    {
+        public Dictionary<ushort, Action<IMessage>> Handlers = new();
 
-{%- for pkt in parser.recv_pkt %}
-    Action< Protocol.{{pkt.name}} > {{pkt.name}}_Handler;
-{%- endfor %}
+        {%- for pkt in parser.recv_pkt %}
+        Action<Protocol.{{pkt.name}} > {{pkt.name}}_Handler;
+        {%- endfor %}
 
-    public PacketHandler()
-    {
-{%- for pkt in parser.recv_pkt %}
-        Handlers.Add({{pkt.id}}, Handle_{{pkt.name}});
-{%- endfor %}
-    }
+        public PacketHandler()
+        {
+            {%- for pkt in parser.recv_pkt %}
+            Handlers.Add({{pkt.id}}, _Handle_{{pkt.name}});
+            {%- endfor %}
+        }
 
-{%- for pkt in parser.recv_pkt %}
-    public void AddHandler( Action<Protocol.{{pkt.name}}> handler )
-    {
-        {{pkt.name}}_Handler += handler;
+        {%- for pkt in parser.recv_pkt %}
+        public void AddHandler( Action<Protocol.{{pkt.name}}> handler )
+        {
+            {{pkt.name}}_Handler += handler;
+        }
+        public void RemoveHandler( Action<Protocol.{{pkt.name}}> handler )
+        {
+            {{pkt.name}}_Handler -= handler;
+        }
+        private void _Handle_{{pkt.name}} (IMessage message )
+        {
+            {{pkt.name}}_Handler?.Invoke((Protocol.{{pkt.name}})message);
+        }
+        {%- endfor %}
     }
-    public void RemoveHandler( Action<Protocol.{{pkt.name}}> handler )
-    {
-        {{pkt.name}}_Handler -= handler;
-    }
-    public void Handle_{{pkt.name}}( IMessage message )
-    {
-        {{pkt.name}}_Handler?.Invoke((Protocol.{{pkt.name}})message);
-    }
-{%- endfor %}
 }
