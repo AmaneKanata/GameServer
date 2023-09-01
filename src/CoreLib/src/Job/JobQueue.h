1a880f7a9ac3d5ae830c01a93564a0a50fc318a9
lhgfrontis <85724878+AmaneKanata@users.noreply.github.com>
2023-09-01 14:05:13 +0900

    Delayed Job 수행 로직 변경

--- src/CoreLib/src/Job/JobQueue.h
+++ src/CoreLib/src/Job/JobQueue.h
@@ -1,7 +1,25 @@
 #pragma once
 
-#include <set>
+#include <queue>
 #include <boost/asio.hpp>
+#include <iostream>
+
+struct DelayedJob
+{
+	DelayedJob(std::chrono::system_clock::time_point time, std::function<void()> func)
+		: time(time)
+		, func(func)
+	{}
+
+	std::chrono::system_clock::time_point time;
+	std::function<void()> func;
+};
+
+struct DelayedJobComparator {
+	bool operator()(const std::shared_ptr<DelayedJob>& lhs, const std::shared_ptr<DelayedJob>& rhs) const {
+		return lhs->time > rhs->time;
+	}
+};
 
 class JobQueue : public std::enable_shared_from_this<JobQueue>
 {
@@ -9,6 +27,7 @@ public:
 	JobQueue(boost::asio::io_context& ioc)
 		: ioc(ioc)
 		, jobs(ioc.get_executor())
+		, delayedJob_timer(ioc)
 	{}
 
 	template<typename T, typename Ret, typename... Args>
@@ -28,72 +47,111 @@ public:
 	}
 
 	template<typename T, typename Ret, typename... Args>
-	std::shared_ptr<boost::asio::steady_timer> DelayPost(int milli, Ret(T::* memFunc)(Args...), Args... args)
+	void DelayPost(int milli, Ret(T::* memFunc)(Args...), Args... args)
 	{
-		auto timer = std::make_shared<boost::asio::steady_timer>(ioc, std::chrono::milliseconds{ milli });
-		{
-			std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
-			delayedJobs.insert(timer);
-		}
-
 		std::shared_ptr<T> owner = std::static_pointer_cast<T>(shared_from_this());
-		timer->async_wait([this, timer, owner, memFunc, args...](const boost::system::error_code& error)
-			{
-				if (!error)
-				{
-					boost::asio::post(jobs, [owner, memFunc, args...]() {
-						(owner.get()->*memFunc)(args...);
-						});
-				}
 
-				std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
-				delayedJobs.erase(timer);
+		auto delayedJob = std::make_shared<DelayedJob>(
+			std::chrono::system_clock::now() + std::chrono::milliseconds(milli),
+			[owner, memFunc, args...]()
+			{
+				(owner.get()->*memFunc)(args...);
 			}
 		);
 
-		return timer;
+		std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
+
+		delayedJobs.push(delayedJob);
+
+		if (delayedJobs.top()->time < delayedJob->time)
+		{
+			return;
+		}
+
+		delayedJob_timer.cancel();
+		delayedJob_timer.expires_at(delayedJobs.top()->time);
+		delayedJob_timer.async_wait([this](const boost::system::error_code& error)
+			{
+				if (!error)
+				{
+					DoDelayedJob();
+				}
+			}
+		);
 	}
 
-	std::shared_ptr<boost::asio::steady_timer> DelayPost(int milli, std::function<void()> func)
+	void DelayPost(int milli, std::function<void()> func)
 	{
-		auto timer = std::make_shared<boost::asio::steady_timer>(ioc, std::chrono::milliseconds{ milli });
+		auto delayedJob = std::make_shared<DelayedJob>(
+			std::chrono::system_clock::now() + std::chrono::milliseconds(milli),
+			func
+		);
+
+		std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
+
+		delayedJobs.push(delayedJob);
+
+		if (delayedJobs.top()->time < delayedJob->time)
 		{
-			std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
-			delayedJobs.insert(timer);
+			return;
 		}
 
-		timer->async_wait([this, timer, func](const boost::system::error_code& error)
+		delayedJob_timer.cancel();
+		delayedJob_timer.expires_at(delayedJobs.top()->time);
+		delayedJob_timer.async_wait([this](const boost::system::error_code& error)
 			{
 				if (!error)
 				{
-					boost::asio::post(jobs, [func]() {
-						func();
-						});
+					DoDelayedJob();
 				}
-
-				std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
-				delayedJobs.erase(timer);
 			}
 		);
+	}
 
-		return timer;
+	void DoDelayedJob()
+	{
+		std::chrono::system_clock::time_point current = std::chrono::system_clock::now();
+
+		std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
+
+		if (!delayedJobs.empty() && delayedJobs.top()->time <= current)
+		{
+			auto job = delayedJobs.top();
+			delayedJobs.pop();
+
+			boost::asio::post(jobs, [job]() {
+				job->func();
+				});
+		}
+
+		if (!delayedJobs.empty())
+		{
+			auto nextJob = delayedJobs.top();
+			delayedJob_timer.cancel();
+			delayedJob_timer.expires_at(nextJob->time);
+			delayedJob_timer.async_wait([this](const boost::system::error_code& error)
+				{
+					if (!error)
+					{
+						DoDelayedJob();
+					}
+				}
+			);
+		}
 	}
 
 	void Cancel(std::shared_ptr<boost::asio::steady_timer> timer)
 	{
-		timer->cancel();
-
-		std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
-		delayedJobs.erase(timer);
+		//TODO
 	}
 
 	void Clear()
 	{
 		std::lock_guard<std::recursive_mutex> lock(delayedJob_mtx);
 
-		for (auto& timer : delayedJobs)
-			timer->cancel();
-		delayedJobs.clear();
+		delayedJob_timer.cancel();
+		while (!delayedJobs.empty())
+			delayedJobs.pop();
 	}
 
 	boost::asio::io_context& GetIoC()
@@ -103,7 +161,10 @@ public:
 
 private:
 	boost::asio::io_context& ioc;
+	
 	boost::asio::strand<boost::asio::io_context::executor_type> jobs;
-	std::set<std::shared_ptr<boost::asio::steady_timer>> delayedJobs;
+	
+	std::priority_queue<std::shared_ptr<DelayedJob>, std::vector<std::shared_ptr<DelayedJob>>, DelayedJobComparator> delayedJobs;
 	std::recursive_mutex delayedJob_mtx;
+	boost::asio::system_timer delayedJob_timer;
 };
