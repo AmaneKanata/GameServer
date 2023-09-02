7c97f42cbfbef9f61fb6794fe3b6959702c9ec06
AmaneKanata <85724878+AmaneKanata@users.noreply.github.com>
2023-09-02 14:19:44 +0900

    환경 변수 설정 변경

--- src/Server/src/Server_Singleton.cpp
+++ src/Server/src/Server_Singleton.cpp
@@ -8,15 +8,15 @@ std::shared_ptr<LogManager> GLogManager = nullptr;
 std::atomic<int> session_num = 0;
 std::atomic<int> client_num = 0;
 
-bool CLOSE_ON_EMPTY = std::getenv("CLOSE_ON_EMPTY") != nullptr ? bool(std::getenv("CLOSE_ON_EMPTY")) : false;
-bool CLOSE_ON_IDLE = std::getenv("CLOSE_ON_IDLE") != nullptr ? bool(std::getenv("CLOSE_ON_IDLE")) : false;
-int CHECK_IDEL_INTERVAL = std::getenv("CHECK_IDEL_INTERVAL") != nullptr ? int(std::getenv("CHECK_IDEL_INTERVAL")) : 10000;
+bool CLOSE_ON_EMPTY = std::getenv("CLOSE_ON_EMPTY") != nullptr ? bool(std::stoi(std::getenv("CLOSE_ON_EMPTY"))) : false;
+bool CLOSE_ON_IDLE = std::getenv("CLOSE_ON_IDLE") != nullptr ? bool(std::stoi(std::getenv("CLOSE_ON_IDLE"))) : false;
+int CHECK_IDEL_INTERVAL = std::getenv("CHECK_IDEL_INTERVAL") != nullptr ? std::stoi(std::getenv("CHECK_IDEL_INTERVAL")) : 10000;
 
-int DISCONNECTED_INTERVAL = std::getenv("DISCONNECTED_INTERVAL") != nullptr ? int(std::getenv("DISCONNECTED_INTERVAL")) : 10000;
-int CHECK_ALIVE_INTERVAL = std::getenv("CHECK_ALIVE_INTERVAL") != nullptr ? int(std::getenv("CHECK_ALIVE_INTERVAL")) : 10000;
+int DISCONNECTED_INTERVAL = std::getenv("DISCONNECTED_INTERVAL") != nullptr ? std::stoi(std::getenv("DISCONNECTED_INTERVAL")) : 10000;
+int CHECK_ALIVE_INTERVAL = std::getenv("CHECK_ALIVE_INTERVAL") != nullptr ? std::stoi(std::getenv("CHECK_ALIVE_INTERVAL")) : 10000;
 
-bool DELAY_SEND = std::getenv("DELAY_SEND") != nullptr ? bool(std::getenv("DELAY_SEND")) : false;
-int DELAY_SEND_INTERVAL = std::getenv("DELAY_SEND_INTERVAL") != nullptr ? int(std::getenv("DELAY_SEND_INTERVAL")) : 0;
+bool DELAY_SEND = std::getenv("DELAY_SEND") != nullptr ? bool(std::stoi(std::getenv("DELAY_SEND"))) : false;
+int DELAY_SEND_INTERVAL = std::getenv("DELAY_SEND_INTERVAL") != nullptr ? std::stoi(std::getenv("DELAY_SEND_INTERVAL")) : 0;
 
 std::shared_ptr<agones::SDK> agones_sdk = nullptr;
 std::string agones_state = "";
